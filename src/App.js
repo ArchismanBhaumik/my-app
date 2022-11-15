8801ed69a48599315d3954b1c06e67b1c3a3a170
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2022-11-15 21:04:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './Navbar';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1 >Welcome to BetterBroker</h1>
+
+    </div>
+  );
+}
+
+export default App;
